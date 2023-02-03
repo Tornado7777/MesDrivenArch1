b57a17f916bacec922f4ea9149342694cd9818c5
Ivan <sample@sample.com>
2023-02-03 20:17:29 +0300

    les1

---
+++ RestoranLes1/Model/Restaurant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestoranLes1.Model
+{
+    public class Restaurant
+    {
+        private readonly List<Table> _tables = new List<Table>();
+        public Restaurant() 
+        {
+            for (ushort i = 1; i <= 10; i++)
+            {
+                _tables.Add(new Table(i));
+            }
+        }
+
+        public void BookFreeTable(int countOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу столи и подтвержу вашу бронь, оставайтесь на линии!");
+            var table = _tables.FirstOrDefault(t => t.SeatsCount > countOfPersons && 
+                                    t.State == State.Free);
+            Thread.Sleep(1000 * 5);
+
+            Console.WriteLine(table is null
+                ? $"К сожалению все столики заняты"
+                : $"Готово! Ваш столик номер {table.Id}.");
+        }
+
+        public void BookFreeTableAsync(int countOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу столи и подтвержу вашу бронь, оставайтесь на линии!");
+            Task.Run(async () => 
+            {
+                var table = _tables.FirstOrDefault(t => t.SeatsCount > countOfPersons &&
+                                   t.State == State.Free);
+                Thread.Sleep(1000 * 5);
+
+                Console.WriteLine(table is null
+                    ? $"УВЕДОМЛЕНИ: К сожалению все столики заняты"
+                    : $"УВЕДОМЛЕНИ: Готово! Ваш столик номер {table.Id}.");
+            });           
+        }
+    }
+}
---
+++ RestoranLes1/Model/Table.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranLes1.Model
+{
+    public class Table
+    {
+        /// <summary>
+        /// Стостояние стола (свободен/занят)
+        /// </summary>
+        public State State { get; private set; }
+
+        /// <summary>
+        /// Кол-во мест за столиком
+        /// </summary>
+        public int SeatsCount { get; }
+
+        /// <summary>
+        /// Номер столика
+        /// </summary>
+        public int Id { get; }
+        public Table(int id) 
+        {
+            var rnd = new Random();
+            Id = id;
+            State = State.Free; // новый стол всегда свободен
+            SeatsCount = rnd.Next(2, 5); //кол-во мест за каждым столиком
+        }
+
+        public bool SetState(State state)
+        {
+            if (state == State)
+            {
+                return false;
+            }
+
+            State = state;
+            return true;
+        }
+    }
+
+    
+    public enum State
+    {
+        /// <summary>
+        /// Стол своббоден
+        /// </summary>
+        Free = 0,
+        /// <summary>
+        /// Стол занят
+        /// </summary>
+        Booked = 1,
+    }
+}
